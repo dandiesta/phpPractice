0ee409a92ded63b4760304e11230e5f3ab1921f7
dandiesta <d.diesta@klab.com>
2015-01-05 14:10:18 +0800

    Substring

---
+++ String/substring_count.php
@@ -0,0 +1,13 @@
+<?php
+
+$sketch = <<< eof
+Well, there's egg and bacon; egg sausage and bacon; egg and spam;
+egg bacon and spam; egg bacon sausage and spam; spam bacon sausage
+and spam; spam egg spam spam bacon and spam; spam sausage spam spam
+bacon spam tomato and spam;
+eof;
+$count = substr_count($sketch, "spam");
+print("The word spam occurs {$count} times.\n");
+
+
+?>
