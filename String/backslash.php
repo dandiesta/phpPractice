bbfdc4308692f27f9e4a9ce6a11de43f764152a8
dandiesta <d.diesta@klab.com>
2015-01-05 13:55:03 +0800

    Adding and Striping Backslashes

---
+++ String/backslash.php
@@ -0,0 +1,11 @@
+<?php
+
+$string = <<< EOF
+"It's never going to work." she cried,
+as she hit the backslash (\) key.
+EOF;
+$string = addslashes($string);
+echo $string;
+echo stripslashes($string);
+
+?>
