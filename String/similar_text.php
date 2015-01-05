61c9e9181791c1991f3a5df9abd4bb9c4f46ca66
dandiesta <d.diesta@klab.com>
2015-01-05 14:09:41 +0800

    Similar text

---
+++ String/similar_text.php
@@ -0,0 +1,8 @@
+<?php
+
+$string1 = "Rasmus Lerdorf";
+$string2 = "Razmus Lehrdorf";
+$common = similar_text($string1, $string2, $percent);
+printf("They have %d chars in common (%.2f%%).\n", $common, $percent);
+
+?>
