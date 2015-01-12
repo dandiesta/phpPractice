2f077810842fc43f6ec2b06bf1d71f5b732c12ac
dandiesta <d.diesta@klab.com>
2015-01-12 13:47:20 +0800

    Handling error output

---
+++ App_Technique/handle_error_output.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<title>Results!</title>
+	</head>
+	<body>
+		<?php
+			function handle_errors ($error, $message, $filename, $line)
+			{
+				ob_end_clean();
+				echo "<b>{$message}</b><br /> in line {$line}<br /> of ";
+				echo "<i>{$filename}</i></body></html>";
+
+				exit;
+			}
+
+			set_error_handler('handle_errors');
+			ob_start();
+		?>
+
+		<h1>Results!</h1>
+
+		<p>Here are the results of your search:</p>
+
+		<table border="1">
+			<?php
+				$db = DB::connect('mysql://gnat:waldu@localhost/webdb');
+
+				if (DB::iserror($db)) {
+					die($db->getMessage());
+				}
+			?>
+		</table>
+	</body>
+</html>
